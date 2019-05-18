3833096106bf58fd713bfdd089682930dbf3eace
resc863 <50689611+resc863@users.noreply.github.com>
2019-05-18 11:59:34 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -540,7 +540,54 @@ async def on_message(message):
             print(duoHeadSky)
             embed.add_field(name='헤드샷,헤드샷상위', value=duoHead + " " + duoHeadSky, inline=False)
             await client.send_message(channel, embed=embed)
+            
+    if message.content.startswith("레식"):
 
+        learn1 = message.content.split(" ")
+        learn = learn1[1]
+        name = learn
+        enc_name = urllib.parse.quote(name)
+        url = "https://r6.tracker.network/profile/pc/"+enc_name
+        html = requests.get(url)
+        bs = BeautifulSoup(html.text, "html.parser")
+        bs_value = bs.find_all("div",{"class": "trn-defstat__value"})
+    
+        lvl1 = bs_value[0]
+        lvl2 = lvl1.text
+        print(lvl2)
+
+        high_mmr1 = bs_value[1]
+        high_mmr2 = high_mmr1.text
+        print(high_mmr2)
+
+        rank = bs_value[2]
+        rank = rank.text
+        print(rank)
+
+        topop1 = bs_value[4]
+        print(topop1)
+
+        pvpwin1 = bs_value[5]
+        pvpwin2 = pvpwin1.text
+        print(pvpwin2)
+
+        winratio1 = bs_value[6]
+        winratio2 = winratio1.text
+        print(winratio2)
+
+        pvpkill1 = bs_value[7]
+        pvpkill2 = pvpkill1.text
+        print(pvpkill2)
+
+        pvpkd1 = bs_value[8]
+        pvpkd2 = pvpkd1.text
+        print(pvpkd2)
+
+        time1 = bs_value[11]
+        time2 = time1.text
+        print(time2)
+        
+        
         
 @client.event
 async def on_member_join(member):
