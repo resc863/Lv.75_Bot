632babada1d39e62d0201d95efc74efae2ecea20
resc863 <50689611+resc863@users.noreply.github.com>
2019-05-19 19:07:02 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -610,6 +610,285 @@ async def on_message(message):
         embed.add_field(name='플레이타임', value=time2, inline=False)
         await client.send_message(channel, embed=embed)
         
+    if message.content.startswith("!연결"):
+        channel = message.author.voice.voice_channel
+        server = message.server
+        voice_client = client.voice_client_in(server)
+        print(voice_client)
+        
+        if voice_client== None:
+            await client.send_message(message.channel, '들어왔습니다') # 호오.... 나를 부르는건가? 네녀석.. 각오는 되있겠지?
+            await client.join_voice_channel(channel)
+        else:
+            await client.send_message(message.channel, '봇이 이미 들어와있습니다.') # 응 이미 들어와있어 응쓰게싸
+
+    if message.content.startswith("!종료"):
+            server = message.server
+            voice_client = client.voice_client_in(server)
+            
+            if voice_client == None:
+                await client.send_message(message.channel,'봇이 음성채널에 접속하지 않았습니다.') # 원래나가있었음 바보녀석 니녀석의 죄는 "어리석음" 이라는 .것.이.다.
+                pass
+            else:
+                await client.send_message(message.channel, '나갑니다') # 나가드림
+                await voice_client.disconnect()
+
+
+import discord
+import asyncio
+import random
+import openpyxl
+from discord import Member
+from discord.ext import commands
+import youtube_dl
+from urllib.request import urlopen, Request
+import urllib
+import urllib.request
+import bs4
+import 급식
+import os
+import sys
+import json
+from selenium import webdriver
+import time
+import datetime
+
+countG = 0
+client = discord.Client()
+players = {}
+queues= {}
+musiclist=[]
+mCount=1
+searchYoutube={}
+searchYoutubeHref={}
+
+def check_queue(id):
+    if queues[id]!=[]:
+        player = queues[id].pop(0)
+        players[id] = player
+        del musiclist[0]
+        player.start()
+
+@client.event
+async def on_ready():
+    print("login")
+    print(client.user.name)
+    print(client.user.id)
+    print("------------------")
+    await client.change_presence(game=discord.Game(name='이런 화창한날에 너같은 녀석은...', type=1))
+
+
+
+@client.event
+async def on_message(message):
+
+    if message.content.startswith('!안녕'):
+        await client.send_message(message.channel, "안녕하세요")
+
+    if message.content.startswith('!오늘배그'):
+        randomNum = random.randrange(1, 3)
+        if randomNum==1:
+            await client.send_message(message.channel, embed=discord.Embed(title="배그각입니다.", color=discord.Color.blue()))
+        else:
+            await client.send_message(message.channel, embed=discord.Embed(title="자러갑시다....", color=discord.Color.red()))
+
+    if message.content.startswith("!홋치"):
+        file = openpyxl.load_workbook('기억.xlsx')
+        sheet = file.active
+        learn = message.content.split(" ")
+        for i in range(1, 201):
+            if sheet["A"+str(i)].value == "-":
+                sheet["A" + str(i)].value = learn[1]
+                sheet["B" + str(i)].value = learn[2]
+                await client.send_message(message.channel, "좀더 때려줘어어엉항항하ㅑㅎ아아")
+                await client.send_message(message.channel, "★ 현재 사용중인 데이터 저장용량 : 200/" + str(i)+" ★")
+                break
+        file.save("기억.xlsx")
+
+    if message.content.startswith("!말해"):
+        file = openpyxl.load_workbook("기억.xlsx")
+        sheet = file.active
+        memory = message.content.split(" ")
+        for i in range(1, 201):
+            if sheet["A" + str(i)].value == memory[1]:
+                await client.send_message(message.channel, sheet["B" + str(i)].value)
+                break
+
+    if message.content.startswith("!기억 초기화") or message.content.startswith("!기억초기화"):
+        file = openpyxl.load_workbook("기억.xlsx")
+        sheet = file.active
+        for i in range(1, 251):
+            sheet["A"+str(i)].value = "-"
+        await client.send_message(message.channel, "기억초기화 완료")
+        file.save("기억.xlsx")
+
+    if message.content.startswith("!데이터목록") or message.content.startswith("!데이터 목록"):
+        file = openpyxl.load_workbook("기억.xlsx")
+        sheet = file.active
+        for i in range(1, 201):
+            if sheet["A" + str(i)].value == "-" and i == 1:
+                await client.send_message(message.channel, "데이터 없음")
+            if sheet["A" + str(i)].value == "-":
+                break
+            await client.send_message(message.channel, "A : "+sheet["A" + str(i)].value + " B : "+ sheet["B" + str(i)].value)
+
+    if message.content.startswith("!명령어"):
+        channel = message.channel
+        embed = discord.Embed(
+            title = '명령어들이다 크크크큭',
+            description = '각각의 명령어들 이다 잘 봐둬라 큭...',
+            colour = discord.Colour.blue()
+        )
+
+        #embed.set_footer(text = '끗')
+        dtime = datetime.datetime.now()
+        #print(dtime[0:4]) # 년도
+        #print(dtime[5:7]) #월
+        #print(dtime[8:11])#일
+        #print(dtime[11:13])#시
+        #print(dtime[14:16])#분
+        #print(dtime[17:19])#초
+        embed.set_footer(text=str(dtime.year)+"년 "+str(dtime.month)+"월 "+str(dtime.day)+"일 "+str(dtime.hour)+"시 "+str(dtime.minute)+"분 "+str(dtime.second)+"초")
+        #embed.set_footer(text=dtime[0:4]+"년 "+dtime[5:7]+"월 "+dtime[8:11]+"일 "+dtime[11:13]+"시 "+dtime[14:16]+"분 "+dtime[17:19]+"초")
+        embed.add_field(name = '!안녕', value = '인사하고싶을때 ㄱㄱ',inline = False)
+        embed.add_field(name='!오늘배그', value='오늘 배그각 알려줌', inline=False)
+        embed.add_field(name='!홋치', value='!홋치 단어1 단어2 형식으로 적으면 학습함', inline=False)
+        embed.add_field(name='!말해', value='!말해 단어1 형식으로 적으면 학습한내용 말함', inline=False)
+        embed.add_field(name='!기억초기화', value='학습한 데이터 초기화함', inline=False)
+        embed.add_field(name='!데이터목록', value='학습한 데이터목록 알려줌', inline=False)
+        embed.add_field(name='!모두모여', value='모두를 언급함', inline=False)
+        embed.add_field(name='!들어와', value='봇이 음성채널에 들어옴', inline=False)
+        embed.add_field(name='!나가', value='봇이 음성채널에 나감', inline=False)
+        embed.add_field(name='!재생', value='!재생 유튜브링크 형식으로 적으면 유튜브 틀어줌', inline=False)
+        embed.add_field(name='!일시정지', value='재생중인 유튜브 일시정지함', inline=False)
+        embed.add_field(name='!다시재생', value='정지중인 유튜브 다시 재생함', inline=False)
+        embed.add_field(name='!멈춰', value='재생,정지중인 유튜브 없어짐(영상목록 초기화)', inline=False)
+        embed.add_field(name='!날씨', value='!날씨 원하는지역 을 입력하면 그 지역의 날씨정보를 제공합니다.', inline=False)
+        embed.add_field(name='!롤', value='!롤 닉네임 형식으로 적으면 그 닉네임에대한 정보를 알려줍니다..', inline=False)
+        embed.add_field(name='!배그솔로', value='!배그솔로 닉네임 형식으로 적으면 그 닉네임에대한 정보를 알려줍니다..', inline=False)
+        embed.add_field(name='!배그듀오', value='!배그듀오 닉네임 형식으로 적으면 그 닉네임에대한 정보를 알려줍니다..', inline=False)
+        embed.add_field(name='!배그스쿼드', value='!배그스쿼드 닉네임 형식으로 적으면 그 닉네임에대한 정보를 알려줍니다..', inline=False)
+        embed.add_field(name='!고양이', value='!고양이 라고 적으면 고양이 사진이 나옵니다..', inline=False)
+        embed.add_field(name='!강아지', value='!강아지 라고 적으면 강아지 사진이 나옵니다.', inline=False)
+        embed.add_field(name='!네코', value='!네코 라고 적으면 2D 고양이 이미지가 나옵니다', inline=False)
+        embed.add_field(name='!실시간검색어, !실검', value='!실시간검색어, !실검 이라고 적으면 네이버의 실시간 검색어 순위가 나타납니다.', inline=False)
+        embed.add_field(name='!번역 번역할문자', value='!번역 번역할문자 이라고 적으면 번역할 문자를 번역한 링크가 나타납니다. ("띄어쓰기를 하시면 안됩니다. _,-등으로 구분해주세요.")', inline=False)
+        embed.add_field(name='!영화순위', value='영화를 1~20순위로 나눈 영화순위 정보를 제공합니다.', inline=False)
+        embed.add_field(name='!급식', value='군포e비즈니스 고등학교의 급식정보를 제공합니다.', inline=False)
+        embed.add_field(name='!복권', value='랜덤으로 선정한 복권번호를 메시지로 보내줍니다.', inline=False)
+        embed.add_field(name='!검색', value='!검색 검색할키워드 형식으로 입력하시면 유튜브 검색결과를 메시지로 보내줍니다.', inline=False)
+
+        await client.send_message(channel,embed=embed)
+
+    if message.content.startswith("!모두모여"):
+        await client.send_message(message.channel, "@everyone")
+
+    if message.content.startswith("!들어와"):
+        channel = message.author.voice.voice_channel
+        server = message.server
+        voice_client = client.voice_client_in(server)
+        print("들어와")
+        print(voice_client)
+        print("들어와")
+        if voice_client== None:
+            await client.send_message(message.channel, '들어왔습니다') # 호오.... 나를 부르는건가? 네녀석.. 각오는 되있겠지?
+            await client.join_voice_channel(channel)
+        else:
+            await client.send_message(message.channel, '봇이 이미 들어와있습니다.') # 응 이미 들어와있어 응쓰게싸
+
+
+
+
+    if message.content.startswith("!나가"):
+            server = message.server
+            voice_client = client.voice_client_in(server)
+            print("나가")
+            print(voice_client)
+            print("나가")
+            if voice_client == None:
+                await client.send_message(message.channel,'봇이 음성채널에 접속하지 않았습니다.') # 원래나가있었음 바보녀석 니녀석의 죄는 "어리석음" 이라는 .것.이.다.
+                pass
+            else:
+                await client.send_message(message.channel, '나갑니다') # 나가드림
+                await voice_client.disconnect()
+
+
+    if message.content.startswith("!play"):
+
+        server = message.server
+        voice_client = client.voice_client_in(server)
+        msg1 = message.content.split(" ")
+        url = msg1[1]
+        player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+        print(player.is_playing())
+        players[server.id] = player
+        await client.send_message(message.channel, embed=discord.Embed(description="재생한다!!!!"))
+        print(player.is_playing())
+        player.start()
+
+
+
+
+    if message.content.startswith("!pause"):
+        id = message.server.id
+        await client.send_message(message.channel, embed=discord.Embed(description="장비를 정비합니다"))
+        players[id].pause()
+
+    if message.content.startswith("!rep"):
+        id = message.server.id
+        await client.send_message(message.channel, embed=discord.Embed(description="다시재생한다!!!!"))
+        players[id].resume()
+
+    if message.content.startswith("!stop"):
+        id = message.server.id
+        await client.send_message(message.channel, embed=discord.Embed(description="세계의 시간은 멈춰있다..."))
+        players[id].stop()
+        print(players[id].is_playing())
+
+    if message.content.startswith('!add'):
+        msg1 = message.content.split(" ")
+        url = msg1[1]
+        server = message.server
+        voice_client = client.voice_client_in(server)
+        player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+        print(player)
+
+        if server.id in queues:
+            queues[server.id].append(player)
+            print('if 1 '+str(queues[server.id])) #queues배열 확인
+        else:
+            queues[server.id] = [player] #딕셔너리 쌍 추가
+            print('else 1' + str(queues[server.id]))#queues배열 확인
+        await client.send_message(message.channel,'예약 완료\n')
+        musiclist.append(url) #대기목록 링크
+
+
+    if message.content.startswith('!list'):
+
+        server = message.server
+        msg1 = message.content.split(" ")
+        mList = msg1[1]
+        num = 0
+        bSize = len(musiclist)
+
+        if mList =='w':
+            embed = discord.Embed(
+                title='대기중인 곡 들',
+                description='대기중.....',
+                colour=discord.Colour.blue()
+            )
+            for i in musiclist:
+                print('예약리스트 : ' + i)
+                embed.add_field(name='대기중인 곡', value=i, inline=False)
+            await client.send_message(message.channel, embed=embed)
+
+        if mList =='c':
+            while num<bSize:
+                del musiclist[0]
+                num = num+1
+
+            del queues[server.id]
+            await client.send_message(message.channel,'예약중인 음악 모두 취소 완료')
         
         
 @client.event
