fdca3ba8a5f8dfa70d7a302139364e7bfb5812e4
resc863 <50689611+resc863@users.noreply.github.com>
2019-05-20 19:42:03 +0900

    Update Lv.75 Bot.js

--- Lv.75 Bot.js
+++ Lv.75 Bot.js
@@ -1,11 +1,8 @@
 function response(room, msg, sender, isGroupChat, replier, ImageDB) { 
-try {
-        if(msg.indexOf("레식") == 0)  {
-        var u = Utils.getWebText("https://r6.tracker.network/profile/pc/"+msg.substr(2));
+      name = msg.split(" ")
+      if(name[0] == "레식")  {
+        var u = Utils.getWebText("https://r6.tracker.network/profile/pc/"+name[1]);
         var t = u.split("<div class=\"trn-defstat__value\">");
-        replier.reply(msg.substr(2) + "님의 레인보우 식스 전적 검색결과 입니다\n레벨 : " + t[0].split("<")[0]+ "\n랭크 : " + t[2].split("<")[0] +"\nMMR : " + t[1].split("<")[0] +"\n승률 : " + t[6].split("<")[0]);\n킬뎃 : " + t[8].split("<")[0]);
-        }
-      }    catch(e) {
-      replier.reply("레식 전적 정보가 없습니다");
-  }
+        replier.reply(name[1] + "님의 레인보우 식스 전적 검색결과 입니다\n레벨 : " + t[0].split("<")[0]+ "\n랭크 : " + t[2].split("<")[0] +"\nMMR : " + t[1].split("<")[0] +"\n승률 : " + t[6].split("<")[0]);\n킬뎃 : " + t[8].split("<")[0]);
+      }
 }
