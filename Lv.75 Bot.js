c01a9acf6eea9c066a3408957301cece120705ac
resc863 <50689611+resc863@users.noreply.github.com>
2019-05-20 17:49:40 +0900

    Update Lv.75 Bot.js

--- Lv.75 Bot.js
+++ Lv.75 Bot.js
@@ -1,11 +1,11 @@
-function response(room, msg, sender, isGroupChat, replier, ImageDB) { //모르면 첫번째 강좌 보고오세요
-try {//예외처리
+function response(room, msg, sender, isGroupChat, replier, ImageDB) { 
+try {
         if(msg.indexOf("레식") == 0)  {
-        var u = Utils.getWebText("https://r6.tracker.network/profile/pc/"+msg.substr(2));//변수 u는 이링크를 HTML파싱한 값이다
+        var u = Utils.getWebText("https://r6.tracker.network/profile/pc/"+msg.substr(2));
         var t = u.split("<div class=\"trn-defstat__value\">");
         replier.reply(msg.substr(2) + "님의 레인보우 식스 전적 검색결과 입니다\n레벨 : " + t[0].split("<")[0]+ "\n랭크 : " + t[2].split("<")[0] +"\nMMR : " + t[1].split("<")[0] +"\n승률 : " + t[6].split("<")[0]);\n킬뎃 : " + t[8].split("<")[0]);
         }
-      }    catch(e) {//결과값을 찾을수 없으면
-      replier.reply("롤전적 정보가 없습니다");
+      }    catch(e) {
+      replier.reply("레식 전적 정보가 없습니다");
   }
 }
