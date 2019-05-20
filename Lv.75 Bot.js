15be57519c46f8275752b7bbc0d5e45a3d1246ff
resc863 <50689611+resc863@users.noreply.github.com>
2019-05-20 19:47:35 +0900

    Update Lv.75 Bot.js

--- Lv.75 Bot.js
+++ Lv.75 Bot.js
@@ -1,8 +1,8 @@
 function response(room, msg, sender, isGroupChat, replier, ImageDB) { 
       name = msg.split(" ")
       if(name[0] == "레식")  {
-        var u = Utils.getWebText("https://r6.tracker.network/profile/pc/"+name[1]);
+        var u = Utils.getWebText("https:/"+"/r6.tracker.network/profile/pc/"+name[1]);
         var t = u.split("<div class=\"trn-defstat__value\">");
-        replier.reply(name[1] + "님의 레인보우 식스 전적 검색결과 입니다\n레벨 : " + t[0].split("<")[0]+ "\n랭크 : " + t[2].split("<")[0] +"\nMMR : " + t[1].split("<")[0] +"\n승률 : " + t[6].split("<")[0]);\n킬뎃 : " + t[8].split("<")[0]);
+        replier.reply(name[1] + "님의 레인보우 식스 전적 검색결과 입니다\n레벨 : " + t[0]+ "\n랭크 : " + t[2] +"\nMMR : " + t[1] +"\n승률 : " + t[6] + "\n킬뎃 : " + t[8];
       }
 }
