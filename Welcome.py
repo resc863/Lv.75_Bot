0e9c306a316b137cc10d0682aa1d7a4aec2ac56d
resc863 <50689611+resc863@users.noreply.github.com>
2019-05-23 21:24:37 +0900

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,6 +1,13 @@
 import discord
 import asyncio
+from discord import Member
 
 client = discord.Client()
 
 @client.event
+async def on_ready():
+    print("Logged in ") 
+    print(client.user.name)
+    print(client.user.id)
+    print("===============")
+    await client.change_presence(game=discord.Game(name=":D", type=1))
