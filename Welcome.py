771170345f32f9f3072bda6e75d9fdc4e3d792e6
resc863 <50689611+resc863@users.noreply.github.com>
2019-05-24 08:11:41 +0900

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -13,9 +13,6 @@ async def on_ready():
     print(client.user.id)
     print("===============")
     await client.change_presence(game=discord.Game(name=":D", type=1))
-    Channel = client.get_channel('role_assignment')
-    Text= "공지를 읽어주시고 아래 반응을 눌러주세요."
-    Moji = await client.send_message(Channel, Text)
 
 @client.event
 async def on_reaction_add(reaction, user):
@@ -25,5 +22,17 @@ async def on_reaction_add(reaction, user):
     if reaction.emoji == ":ok_hand:":
       Role = discord.utils.get(user.server.roles, name="[guest]")
       await client.add_roles(user, Role)
+
+@client.event
+async def on_member_join(member):
+    fmt = '{1.name} 에 오신것을 환영합니다., {0.mention} 님'
+    channel = member.server.get_channel("chat")
+    await client.send_message(channel, fmt.format(member, member.server))
+    await client.send_message(member, "반갑습니다 <@"+id+">님.")
+    Text= "공지를 읽어주시고 아래 반응을 눌러주세요."
+    Moji = await client.send_message(Channel, Text)
+    await client.add_reaction(Moji, emoji=':ok_hand:')
+
+
 
 client.run(token)
