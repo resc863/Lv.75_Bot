a2e4613c55281e959b07b2357ba829bee7509413
resc863 <50689611+resc863@users.noreply.github.com>
2019-05-23 21:31:38 +0900

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -12,3 +12,15 @@ async def on_ready():
     print(client.user.id)
     print("===============")
     await client.change_presence(game=discord.Game(name=":D", type=1))
+    Channel = client.get_channel('role_assignment')
+    Text= "공지를 읽어주시고 아래 반응을 눌러주세요."
+    Moji = await client.send_message(Channel, Text)
+
+@client.event
+async def on_reaction_add(reaction, user):
+    Channel = client.get_channel('notice')
+    if reaction.message.channel.id != Channel
+    return
+    if reaction.emoji == ":ok_hand:":
+      Role = discord.utils.get(user.server.roles, name="[guest]")
+      await client.add_roles(user, Role)
