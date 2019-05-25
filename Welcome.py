b154e5b0fc742252f804c37e4d50ccaada8e33f7
resc863 <resc863@gmail.com>
2019-05-25 17:21:01 +0900

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -6,7 +6,7 @@ from discord import Member
 from discord.ext import commands
 
 client = discord.Client()
-token = os.environ(key)
+token = os.environ["key"]
 
 @client.event
 async def on_ready():
@@ -20,9 +20,9 @@ async def on_ready():
 async def on_member_join(member):
     fmt = '{1.name} 에 오신것을 환영합니다., {0.mention} 님. '
     channel = member.server.get_channel("450316317988356100")
-    role = discord.utils.get(server.roles, id="450322259660374036")
+    role = discord.utils.get(member.server.roles, id="450322259660374036")
     await client.send_message(channel, fmt.format(member, member.server))
-    await client.send_message(member, "반갑습니다. <@"+id+">님.")
+    await client.send_message(member, "반갑습니다.")
     await client.add_roles(member, role)
 
 client.run(token)
