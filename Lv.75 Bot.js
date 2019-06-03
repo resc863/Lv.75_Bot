d91c1b2f37424f3054ff3b421150bc1bee2e6611
resc863 <50689611+resc863@users.noreply.github.com>
2019-06-03 12:55:51 +0900

    Update Lv.75 Bot.js

--- Lv.75 Bot.js
+++ Lv.75 Bot.js
@@ -1,8 +1,5 @@
-function response(room, msg, sender, isGroupChat, replier, ImageDB) { 
-      name = msg.split(" ")
-      if(name[0] == "레식")  {
-        var u = Utils.getWebText("https:/"+"/r6.tracker.network/profile/pc/"+name[1]);
-        var t = u.split("<div class=\"trn-defstat__value\">");
-        replier.reply(name[1] + "님의 레인보우 식스 전적 검색결과 입니다\n레벨 : " + t[0]+ "\n랭크 : " + t[2] +"\nMMR : " + t[1] +"\n승률 : " + t[6] + "\n킬뎃 : " + t[8];
-      }
-}
+var json = getWebText("https://api.r6stats.com/api/v1/players/"+name+"/?platform=uplay")
+
+var data =JSON.parse(json)
+
+var lvl = data.find
