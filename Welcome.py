8795930906962b560010e6deb8420a563de8f9c9
resc863 <50689611+resc863@users.noreply.github.com>
2019-06-05 11:58:26 +0900

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -21,7 +21,13 @@ async def on_ready():
     print(client.user.id)
     print("===============")
     
-    channel = client.get_channel('581685214422761492')
+@client.event
+async def on_member_join(member):
+    fmt = '{1.name} 에 오신것을 환영합니다., {0.mention} 님. '
+    channel = member.server.get_channel("450316317988356100")
+    role = discord.utils.get(member.server.roles, id="450322259660374036")
+    await client.add_roles(member, role)
+
     global moji1
     global moji2
     global moji3
@@ -44,7 +50,7 @@ async def on_reaction_add(reaction, user):
     global moji2
     global moji3
     
-    channel = client.get_channel('581685214422761492')
+    channel = client.get_channel('450316317988356100')
 
     if reaction.emoji == "\U0001F1F7":
         role = discord.utils.get(user.server.roles, id="460137279533481984")
@@ -71,12 +77,5 @@ async def on_reaction_remove(reaction, user):
     if reaction.emoji == "\U0001F3AE":
         role = discord.utils.get(user.server.roles, id="485720562472189952")
         await client.remove_roles(user, role)
-
-@client.event
-async def on_member_join(member):
-    fmt = '{1.name} 에 오신것을 환영합니다., {0.mention} 님. '
-    channel = member.server.get_channel("450316317988356100")
-    role = discord.utils.get(member.server.roles, id="450322259660374036")
-    await client.add_roles(member, role)
 
 client.run(token)
