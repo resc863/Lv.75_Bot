fb138f8a637610a9ea690e39a4c4da4f6dbf5f76
resc863 <resc863@gmail.com>
2019-07-05 17:20:05 +0900

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -24,7 +24,6 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     fmt = '{1.name} 에 오신것을 환영합니다., {0.mention} 님. '
-    channel = member.server.get_channel("450316317988356100")
     role = discord.utils.get(member.server.roles, id="450322259660374036")
     await client.add_roles(member, role)
 
@@ -32,25 +31,58 @@ async def on_member_join(member):
     
     role = discord.utils.get(member.server.roles, id="460137279533481984")
     await client.add_roles(member, role)
+    
+    channel = client.get_channel('592296776850210816')
 
-    game = "레인보우 식스 하시면 눌러주세요"
+
+    message = await client.send_message(channel, "레인보우 식스를 하신다면 누르세요.")
+    await client.add_reaction(message, emoji="\U0001F44F")
+
+    message = await client.send_message(channel, "레인보우 식스 정보를 수신하시겠습니까? 그럼 누르세요.")
+    await client.add_reaction(message, emoji="\U0001F600")
+
+    message = await client.send_message(channel, "캐주얼 팟 모집을 원하시면 누르세요.")
+    await client.add_reaction(message, emoji="\U0001F64F")
+
+    message = await client.send_message(channel, "랭킹 팟 모집을 원하시면 누르세요.")
+    await client.add_reaction(message, emoji="\U0001F932")
 
 @client.event
 async def on_reaction_add(reaction, user):
-
-    global moji3
-    
-    channel = client.get_channel('450316317988356100')
         
-    if reaction.emoji == "\U0001F3AE":
-        role = discord.utils.get(user.server.roles, id="485720562472189952")
+    if reaction.emoji == "\U0001F44F":
+        role = discord.utils.get(user.server.roles, id="592296285994745856")
+        await client.add_roles(user, role)
+
+    if reaction.emoji == "\U0001F600":
+        role = discord.utils.get(user.server.roles, id="592295677992632320")
+        await client.add_roles(user, role)
+
+    if reaction.emoji == "\U0001F64F":
+        role = discord.utils.get(user.server.roles, id="592296204633636864")
+        await client.add_roles(user, role)
+
+    if reaction.emoji == "\U0001F932":
+        role = discord.utils.get(user.server.roles, id="592296204633636864")
         await client.add_roles(user, role)
         
 @client.event
 async def on_reaction_remove(reaction, user):
-        
-    if reaction.emoji == "\U0001F3AE":
-        role = discord.utils.get(user.server.roles, id="485720562472189952")
+
+    if reaction.emoji == "\U0001F44F":
+        role = discord.utils.get(user.server.roles, id="592296285994745856")
+        await client.remove_roles(user, role)
+
+    if reaction.emoji == "\U0001F600":
+        role = discord.utils.get(user.server.roles, id="592295677992632320")
+        await client.remove_roles(user, role)
+
+    if reaction.emoji == "\U0001F64F":
+        role = discord.utils.get(user.server.roles, id="592296204633636864")
+        await client.remove_roles(user, role)
+
+    if reaction.emoji == "\U0001F932":
+        role = discord.utils.get(user.server.roles, id="592296204633636864")
         await client.remove_roles(user, role)
 
 client.run(token)
