7de65ac9bde052ecef470a6497a962ba66fbf8cf
resc863 <50689611+resc863@users.noreply.github.com>
2019-09-04 22:20:36 +0900

    Delete matrix.bat

--- matrix.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title matrix
-color 0a
-
-mode 1000
-
-:matrix
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
-goto matrix
