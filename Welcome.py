a30afe14479b2ecaa2c0cfe96a66f274801fc585
resc863 <50689611+resc863@users.noreply.github.com>
2019-10-28 16:37:49 +0900

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -25,50 +25,44 @@ async def on_message(message):
 
         embed = discord.Embed(title="레인보우 식스", description="레인보우 식스 관련 역할", color=0x00ff00)
         embed.add_field(name="멘션 허용", value="레인보우 식스 관련 멘션을 받으려면 첫번째 버튼을 누르세요", inline=False)
-        embed.add_field(name="멘션 거부", value="레인보우 식스 관련 멘션을 받지 않으려면 첫번째 버튼을 누르세요", inline=False)
-        message = embed
-        await client.send_message(channel, embed=embed)
+        embed.add_field(name="멘션 거부", value="레인보우 식스 관련 멘션을 받지 않으려면 두번째 버튼을 누르세요", inline=False)
+        message = await client.send_message(channel, embed=embed)
         await client.add_reaction(message, emoji="\U0001F600")
         await client.add_reaction(message, emoji="\U0001F64F")
         
         embed = discord.Embed(title="데스티니 2", description="데스티니 2 관련 역할", color=0x00ff00)
         embed.add_field(name="멘션 허용", value="데스티니 2 관련 멘션을 받으려면 첫번째 버튼을 누르세요", inline=False)
-        embed.add_field(name="멘션 거부", value="데스티니 2 관련 멘션을 받지 않으려면 첫번째 버튼을 누르세요", inline=False)
-        message = embed
-        await client.send_message(channel, embed=embed)
+        embed.add_field(name="멘션 거부", value="데스티니 2 관련 멘션을 받지 않으려면 두번째 버튼을 누르세요", inline=False)
+        message = await client.send_message(channel, embed=embed)
         await client.add_reaction(message, emoji="\U0001F910")
         await client.add_reaction(message, emoji="\U0001F44F")
         
         embed = discord.Embed(title="GTA5", description="GTA5 관련 역할", color=0x00ff00)
         embed.add_field(name="멘션 허용", value="GTA5 관련 멘션을 받으려면 첫번째 버튼을 누르세요", inline=False)
-        embed.add_field(name="멘션 거부", value="GTA5 관련 멘션을 받지 않으려면 첫번째 버튼을 누르세요", inline=False)
-        message = embed
-        await client.send_message(channel, embed=embed)
+        embed.add_field(name="멘션 거부", value="GTA5 관련 멘션을 받지 않으려면 두번째 버튼을 누르세요", inline=False)
+        message = await client.send_message(channel, embed=embed)
         await client.add_reaction(message, emoji="\U0001F91D")
         await client.add_reaction(message, emoji="\U0001F91A")
 
         embed = discord.Embed(title="마인크래프트", description="마인크래프트 관련 역할", color=0x00ff00)
         embed.add_field(name="멘션 허용", value="마인크래프트 관련 멘션을 받으려면 첫번째 버튼을 누르세요", inline=False)
-        embed.add_field(name="멘션 거부", value="마인크래프트 관련 멘션을 받지 않으려면 첫번째 버튼을 누르세요", inline=False)
-        message = embed
-        await client.send_message(channel, embed=embed)
+        embed.add_field(name="멘션 거부", value="마인크래프트 관련 멘션을 받지 않으려면 두번째 버튼을 누르세요", inline=False)
+        message = await client.send_message(channel, embed=embed)
         await client.add_reaction(message, emoji="\U0001F590")
-        await client.add_reaction(message, emoji="\U00002706")
+        await client.add_reaction(message, emoji="\U0001F488")
 
         embed = discord.Embed(title="콜오브듀티", description="콜오브듀티 관련 역할", color=0x00ff00)
         embed.add_field(name="멘션 허용", value="콜오브듀티 관련 멘션을 받으려면 첫번째 버튼을 누르세요", inline=False)
-        embed.add_field(name="멘션 거부", value="콜오브듀티 관련 멘션을 받지 않으려면 첫번째 버튼을 누르세요", inline=False)
-        message = embed
-        await client.send_message(channel, embed=embed)
-        await client.add_reaction(message, emoji="\U0001F440")
-        await client.add_reaction(message, emoji="\U0001F90F")
+        embed.add_field(name="멘션 거부", value="콜오브듀티 관련 멘션을 받지 않으려면 두번째 버튼을 누르세요", inline=False)
+        message = await client.send_message(channel, embed=embed)
+        await client.add_reaction(message, emoji="\U0001F5A5")
+        await client.add_reaction(message, emoji="\U00002328")
 
     if message.content.startswith("!가이드"):  
         channel = client.get_channel('638039195234992155')
 
         embed = discord.Embed(title="안내", description="다음을 읽고 버튼을 눌러 주십시오.", color=0x00ff00)
-        message = embed
-        await client.send_message(channel, embed=embed)
+        message = await client.send_message(channel, embed=embed)
         await client.add_reaction(message, emoji="\U0001F446")
 
 @client.event
@@ -106,15 +100,15 @@ async def on_reaction_add(reaction, user):
         role = discord.utils.get(user.server.roles, id="638036108235636746")
         await client.add_roles(user, role)
     
-    if reaction.emoji == "\U00002706":
+    if reaction.emoji == "\U0001F488":
         role = discord.utils.get(user.server.roles, id="638038347746377768")
         await client.add_roles(user, role)
 
-    if reaction.emoji == "\U0001F440":
+    if reaction.emoji == "\U0001F5A5":
         role = discord.utils.get(user.server.roles, id="638035923799375893")
         await client.add_roles(user, role)
 
-    if reaction.emoji == "\U0001F90F":
+    if reaction.emoji == "\U00002328":
         role = discord.utils.get(user.server.roles, id="638038402297626634")
         await client.add_roles(user, role)
         
@@ -153,15 +147,15 @@ async def on_reaction_remove(reaction, user):
         role = discord.utils.get(user.server.roles, id="638036108235636746")
         await client.remove_roles(user, role)
     
-    if reaction.emoji == "\U00002706":
+    if reaction.emoji == "\U0001F488":
         role = discord.utils.get(user.server.roles, id="638038347746377768")
         await client.remove_roles(user, role)
 
-    if reaction.emoji == "\U0001F440":
+    if reaction.emoji == "\U0001F5A5":
         role = discord.utils.get(user.server.roles, id="638035923799375893")
         await client.remove_roles(user, role)
 
-    if reaction.emoji == "\U0001F90F":
+    if reaction.emoji == "\U00002328":
         role = discord.utils.get(user.server.roles, id="638038402297626634")
         await client.remove_roles(user, role)
         
