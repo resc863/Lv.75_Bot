db16a531063758b91e96089f59402a1b1dcaa993
resc863 <50689611+resc863@users.noreply.github.com>
2019-10-28 14:35:54 +0900

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -6,10 +6,6 @@ from discord import Member
 from discord.ext import commands
 from itertools import cycle
 
-moji1=None
-moji2=None
-moji3=None
-
 client = discord.Client()
 
 token = os.environ["key"]
@@ -19,86 +15,156 @@ async def on_ready():
     print("Logged in ") 
     print(client.user.name)
     print(client.user.id)
-    print("===============")
+    print("====================")
 
 @client.event
 async def on_message(message):
     if message.content.startswith("!역할"):
         
         channel = client.get_channel('592296776850210816')
-        message = await client.send_message(channel, "레인보우 식스를 하신다면 누르세요.")
 
-        await client.add_reaction(message, emoji="\U0001F44F")
-        message = await client.send_message(channel, "레인보우 식스 정보를 수신하시려면 누르세요.")
-        
+        embed = discord.Embed(title="레인보우 식스", description="레인보우 식스 관련 역할", color=0x00ff00)
+        embed.add_field(name="멘션 허용", value="레인보우 식스 관련 멘션을 받으려면 첫번째 버튼을 누르세요", inline=False)
+        embed.add_field(name="멘션 거부", value="레인보우 식스 관련 멘션을 받지 않으려면 첫번째 버튼을 누르세요", inline=False)
+        message = embed
+        await client.send_message(channel, embed=embed)
         await client.add_reaction(message, emoji="\U0001F600")
-        
-        message = await client.send_message(channel, "캐주얼 팟 모집을 원하시면 누르세요.")
         await client.add_reaction(message, emoji="\U0001F64F")
         
-        message = await client.send_message(channel, "랭킹 팟 모집을 원하시면 누르세요.")
+        embed = discord.Embed(title="데스티니 2", description="데스티니 2 관련 역할", color=0x00ff00)
+        embed.add_field(name="멘션 허용", value="데스티니 2 관련 멘션을 받으려면 첫번째 버튼을 누르세요", inline=False)
+        embed.add_field(name="멘션 거부", value="데스티니 2 관련 멘션을 받지 않으려면 첫번째 버튼을 누르세요", inline=False)
+        message = embed
+        await client.send_message(channel, embed=embed)
         await client.add_reaction(message, emoji="\U0001F910")
+        await client.add_reaction(message, emoji="\U0001F44F")
         
-        await client.send_message(channel, "-----------------------------------------")
-        
-        message = await client.send_message(channel, "GTA5를 하신다면 누르세요.")
+        embed = discord.Embed(title="GTA5", description="GTA5 관련 역할", color=0x00ff00)
+        embed.add_field(name="멘션 허용", value="GTA5 관련 멘션을 받으려면 첫번째 버튼을 누르세요", inline=False)
+        embed.add_field(name="멘션 거부", value="GTA5 관련 멘션을 받지 않으려면 첫번째 버튼을 누르세요", inline=False)
+        message = embed
+        await client.send_message(channel, embed=embed)
         await client.add_reaction(message, emoji="\U0001F91D")
-    
-    
-@client.event
-async def on_member_join(member):
-    fmt = '{1.name} 에 오신것을 환영합니다., {0.mention} 님. '
-    role = discord.utils.get(member.server.roles, id="450322259660374036")
-    await client.add_roles(member, role)
-    
-    role = discord.utils.get(member.server.roles, id="460137279533481984")
-    await client.add_roles(member, role)
-    
+        await client.add_reaction(message, emoji="\U0001F91A")
+
+        embed = discord.Embed(title="마인크래프트", description="마인크래프트 관련 역할", color=0x00ff00)
+        embed.add_field(name="멘션 허용", value="마인크래프트 관련 멘션을 받으려면 첫번째 버튼을 누르세요", inline=False)
+        embed.add_field(name="멘션 거부", value="마인크래프트 관련 멘션을 받지 않으려면 첫번째 버튼을 누르세요", inline=False)
+        message = embed
+        await client.send_message(channel, embed=embed)
+        await client.add_reaction(message, emoji="\U0001F590")
+        await client.add_reaction(message, emoji="\U00002706")
+
+        embed = discord.Embed(title="콜오브듀티", description="콜오브듀티 관련 역할", color=0x00ff00)
+        embed.add_field(name="멘션 허용", value="콜오브듀티 관련 멘션을 받으려면 첫번째 버튼을 누르세요", inline=False)
+        embed.add_field(name="멘션 거부", value="콜오브듀티 관련 멘션을 받지 않으려면 첫번째 버튼을 누르세요", inline=False)
+        message = embed
+        await client.send_message(channel, embed=embed)
+        await client.add_reaction(message, emoji="\U0001F440")
+        await client.add_reaction(message, emoji="\U0001F90F")
+
+    if message.content.startswith("!가이드"):  
+        channel = client.get_channel('638039195234992155')
+
+        embed = discord.Embed(title="안내", description="다음을 읽고 버튼을 눌러 주십시오.", color=0x00ff00)
+        message = embed
+        await client.send_message(channel, embed=embed)
+        await client.add_reaction(message, emoji="\U0001F446")
 
 @client.event
 async def on_reaction_add(reaction, user):
+
+    if reaction.emoji == "\U0001F446":
+        role = discord.utils.get(user.server.roles, id="450322259660374036")
+        await client.add_roles(user, role)
         
     if reaction.emoji == "\U0001F44F":
-        role = discord.utils.get(user.server.roles, id="592296285994745856")
+        role = discord.utils.get(user.server.roles, id="638038286392098818")
         await client.add_roles(user, role)
 
     if reaction.emoji == "\U0001F600":
-        role = discord.utils.get(user.server.roles, id="592295677992632320")
+        role = discord.utils.get(user.server.roles, id="592295687786594305")
         await client.add_roles(user, role)
 
     if reaction.emoji == "\U0001F64F":
-        role = discord.utils.get(user.server.roles, id="592296204633636864")
+        role = discord.utils.get(user.server.roles, id="592296285994745856")
         await client.add_roles(user, role)
 
     if reaction.emoji == "\U0001F910":
-        role = discord.utils.get(user.server.roles, id="592296204633636864")
+        role = discord.utils.get(user.server.roles, id="638035231349276672")
         await client.add_roles(user, role)
     
     if reaction.emoji == "\U0001F91D":
         role = discord.utils.get(user.server.roles, id="607893687472750608")
+        await client.add_roles(user, role)
+    
+    if reaction.emoji == "\U0001F91A":
+        role = discord.utils.get(user.server.roles, id="638038441010921484")
+        await client.add_roles(user, role)
+    
+    if reaction.emoji == "\U0001F590":
+        role = discord.utils.get(user.server.roles, id="638036108235636746")
+        await client.add_roles(user, role)
+    
+    if reaction.emoji == "\U00002706":
+        role = discord.utils.get(user.server.roles, id="638038347746377768")
+        await client.add_roles(user, role)
+
+    if reaction.emoji == "\U0001F440":
+        role = discord.utils.get(user.server.roles, id="638035923799375893")
+        await client.add_roles(user, role)
+
+    if reaction.emoji == "\U0001F90F":
+        role = discord.utils.get(user.server.roles, id="638038402297626634")
         await client.add_roles(user, role)
         
 @client.event
 async def on_reaction_remove(reaction, user):
 
+    if reaction.emoji == "\U0001F446":
+        role = discord.utils.get(user.server.roles, id="450322259660374036")
+        await client.remove_roles(user, role)
+        
     if reaction.emoji == "\U0001F44F":
-        role = discord.utils.get(user.server.roles, id="592296285994745856")
+        role = discord.utils.get(user.server.roles, id="638038286392098818")
         await client.remove_roles(user, role)
 
     if reaction.emoji == "\U0001F600":
-        role = discord.utils.get(user.server.roles, id="592295677992632320")
+        role = discord.utils.get(user.server.roles, id="592295687786594305")
         await client.remove_roles(user, role)
 
     if reaction.emoji == "\U0001F64F":
-        role = discord.utils.get(user.server.roles, id="592296204633636864")
+        role = discord.utils.get(user.server.roles, id="592296285994745856")
         await client.remove_roles(user, role)
 
     if reaction.emoji == "\U0001F910":
-        role = discord.utils.get(user.server.roles, id="592296204633636864")
+        role = discord.utils.get(user.server.roles, id="638035231349276672")
         await client.remove_roles(user, role)
     
     if reaction.emoji == "\U0001F91D":
         role = discord.utils.get(user.server.roles, id="607893687472750608")
         await client.remove_roles(user, role)
+    
+    if reaction.emoji == "\U0001F91A":
+        role = discord.utils.get(user.server.roles, id="638038441010921484")
+        await client.remove_roles(user, role)
+    
+    if reaction.emoji == "\U0001F590":
+        role = discord.utils.get(user.server.roles, id="638036108235636746")
+        await client.remove_roles(user, role)
+    
+    if reaction.emoji == "\U00002706":
+        role = discord.utils.get(user.server.roles, id="638038347746377768")
+        await client.remove_roles(user, role)
+
+    if reaction.emoji == "\U0001F440":
+        role = discord.utils.get(user.server.roles, id="638035923799375893")
+        await client.remove_roles(user, role)
+
+    if reaction.emoji == "\U0001F90F":
+        role = discord.utils.get(user.server.roles, id="638038402297626634")
+        await client.remove_roles(user, role)
+        
+    
 
 client.run(token)
