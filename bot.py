675a0e5326733f1493d1998bdfde1dc67fa3e202
resc863 <50689611+resc863@users.noreply.github.com>
2020-01-27 16:31:22 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ def search_book(keyword):
 
     url =  base_url + urllib.parse.urlencode(params, encoding = encoding_type)
     url_get = requests.get(url)
-    soup = BeautifulSoup(url_get.content, 'lxml')
+    soup = BeautifulSoup(url_get.content, 'html.parser')
     items = soup.find_all(class_='ss_book_box')
     list = []
 
